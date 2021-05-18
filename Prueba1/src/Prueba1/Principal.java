26af44de8a1cb41b9043a5e2d6490fa3c916c009
Pilar <Pilar@PILI-LAP>
2021-05-18 14:56:57 +0200

    Principal cambio

--- Prueba1/src/Prueba1/Principal.java
+++ Prueba1/src/Prueba1/Principal.java
@@ -2,6 +2,7 @@ package Prueba1;
 
 public class Principal {
 	public static void main(String[]args) {
-		System.out.println("Hola mundo java-git"); 
+		System.out.println("Hola mundo java-git");
+		System.out.println("Prueba Git. Se añade cambio1"); 
 	}
 }
